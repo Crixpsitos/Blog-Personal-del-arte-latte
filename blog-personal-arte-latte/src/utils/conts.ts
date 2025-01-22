02a12b4568a01791b82ed99dfb4c193c4c52e7a7
Criixpsitos <cristianpee2609@gmail.com>
2025-01-21 22:00:01 -0500

    inicio del proyecto del proyecto de arte latte

---
+++ blog-personal-arte-latte/src/utils/conts.ts
@@ -0,0 +1,8 @@
+import { Home, Coffee, Image, Phone } from "lucide-astro";
+
+export const navItems = [
+    { name: "Inicio", href: "/", icon: Home },
+    { name: "Blog", href: "/blog", icon: Coffee },
+    { name: "Galería", href: "/galeria", icon: Image },
+    { name: "Sobre mí", href: "/sobre-mi", icon: Phone },
+  ];
